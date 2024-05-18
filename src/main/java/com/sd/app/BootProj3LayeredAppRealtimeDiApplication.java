5adb2ae86ecd8d1240519fd028111c4d8883a956
sumit-dakua <sumitdakua4@gmail.com>
2024-05-18 17:53:50 +0530

    initial commit

---
+++ src/main/java/com/sd/app/BootProj3LayeredAppRealtimeDiApplication.java
@@ -0,0 +1,13 @@
+package com.sd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj3LayeredAppRealtimeDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj3LayeredAppRealtimeDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sd/app/BootProj3LayeredAppRealtimeDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProj3LayeredAppRealtimeDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
